69c19e57572282284c17a89be8ef159f7408370a
sachinm78 <sachinm78@gmail.com>
2019-08-22 13:55:35 -0500

    created sample data and successfully rendered simple component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Joke from "./Joke"
+import jokesData from "./jokesData"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+    
+    return (
+        <div>
+            {jokeComponents}            
+        </div>
+    )
 }
 
-export default App;
+export default App
